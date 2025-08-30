9f388c3806f869cef41c41117fd0ce3a8e55fc19
Jake Go <jake@notnotjake.com>
2025-08-30 03:12:15 -0400

    Enable remotes

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
+	compilerOptions: {
+		experimental: {
+			async: true
+		}
+	},
 	kit: {
 		adapter: adapter(),
+		experimental: {
+			remoteFunctions: true
+		},
 		alias: {
 			$ui: 'src/lib/components',
 			$utils: 'src/lib/utils',
