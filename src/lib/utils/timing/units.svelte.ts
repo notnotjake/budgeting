63ccd4db23556b6107d845f72683ad3ef7e32ee0
Jake Go <jake@notnotjake.com>
2025-10-09 02:05:29 -0400

    add timing utils

---
+++ src/lib/utils/timing/units.svelte.ts
@@ -0,0 +1,11 @@
+export const MILLISECOND = 1
+export const MS = MILLISECOND
+
+export const SECOND = 1000
+export const SEC = SECOND
+
+export const MINUTE = 60 * SECOND
+export const MIN = MINUTE
+
+export const HOUR = 60 * MINUTE
+export const HR = HOUR
