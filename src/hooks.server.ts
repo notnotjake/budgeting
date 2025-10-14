a245153f2b49a10528ce5582297371e063a883a5
Jake Go <jake@notnotjake.com>
2025-10-14 01:57:21 -0400

    updated to use auth hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,10 +2,9 @@ import type { Handle } from '@sveltejs/kit'
 import { sequence } from '@sveltejs/kit/hooks'
 
 import { ratelimit } from '$lib/server/ratelimit'
+import Auth from '$lib/server/auth'
 
-// import { authHandler } from '$lib/server/auth'
-
-export const serverHook: Handle = async ({ event, resolve }) => {
+export const handleGlobalRatelimit: Handle = async ({ event, resolve }) => {
 	// Your hook code
 	const ip = event.getClientAddress()
 	const result = await ratelimit.free.limit(ip)
@@ -15,4 +14,8 @@ export const serverHook: Handle = async ({ event, resolve }) => {
 	return resolve(event)
 }
 
-export const handle: Handle = sequence(serverHook)
+export const handle: Handle = sequence(
+	handleGlobalRatelimit,
+	Auth.hooks.handleAuthentication,
+	Auth.hooks.handleProtected
+)
