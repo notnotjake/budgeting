1faed75852e4603cd75929e21c234237826ef42d
Jake Go <jake@notnotjake.com>
2025-10-28 02:03:22 -0400

    move login button on index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,10 @@
 </script>
 
 <div class="max-w-160 mx-auto flex min-h-full flex-col gap-2 bg-neutral-50 px-6 py-8">
+	<div class="W-full flex justify-end">
+		<Button href="/login" style="primary" rounded="full" class="w-fit bg-neutral-600">Login</Button>
+	</div>
+
 	<h1 class="text-lg font-medium">SvelteKit Example</h1>
 
 	<div
@@ -13,6 +17,4 @@
 			using your email for a passwordless experience, or you can add a passkey.
 		</p>
 	</div>
-
-	<Button href="/login" style="primary" rounded="md" class="w-fit">Go to Login</Button>
 </div>
