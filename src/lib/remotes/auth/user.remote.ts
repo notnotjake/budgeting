874f3be0d7e41e9ce87e059552c6adcd7ac71961
Jake Go <jake@notnotjake.com>
2025-11-01 01:25:16 -0400

    delete account remote

--- src/lib/remotes/auth/user.remote.ts
+++ src/lib/remotes/auth/user.remote.ts
@@ -1,3 +1,10 @@
+import { form, query, command, getRequestEvent } from '$app/server'
+import { error, redirect } from '@sveltejs/kit'
+import { z } from 'zod'
+import Auth from '$lib/server/auth'
+import AuthCore from '$lib/server/auth/core'
+import { AuthEmails } from '$lib/server/auth'
+
 // updateUserProfile
 // startEmailChange
 // verifyEmailChange
@@ -5,3 +12,42 @@
 // lockUserAccount
 // unlockUserAccount
 // deleteUserAccount
+export const deleteUserAccount = command(async () => {
+	const event = getRequestEvent()
+
+	// ensure recent auth
+	const session = event.locals.session
+	const user = event.locals.user
+
+	if (!user || !session) {
+		throw error(401)
+	}
+
+	const hasRecentAuth =
+		session?.lastAuthAt &&
+		Date.now() < session.lastAuthAt.getTime() + Auth.durations.recentAuthWindow
+
+	if (!hasRecentAuth) {
+		AuthCore.setRedirectUrlCookie(event)
+		redirect(303, Auth.routes.reauth)
+	}
+
+	// delete user
+	const result = await AuthCore.deleteUser({ userId: user.id })
+
+	if (!result.success) {
+		throw error(500)
+	}
+
+	// invalidate session
+	await AuthCore.invalidateSession(session.id)
+
+	AuthCore.clearRedirectUrlCookie(event)
+	AuthCore.clearSessionTokenCookie(event)
+
+	// send email
+	await AuthEmails.sendAccountDeletionCompleted({ email: user.identifier })
+
+	// return success
+	return { success: true }
+})
