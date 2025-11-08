d6a563188c7278874282a2c38d62a1143fe917ab
Jake Go <jake@notnotjake.com>
2025-11-08 18:08:29 -0500

    textarea input component

---
+++ src/lib/components/input/textarea.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { TextareaAutosize } from 'runed'
+	import { createClass } from '@opensky/style'
+
+	type Props = { class: string; value: string; placeholder?: string; maxHeight?: number }
+	let {
+		class: classProp,
+		value = $bindable(''),
+		placeholder = 'Start writing',
+		maxHeight = 400
+	}: Props = $props()
+
+	let el = $state<HTMLTextAreaElement>(null!)
+
+	new TextareaAutosize({
+		element: () => el,
+		input: () => value,
+		maxHeight: maxHeight
+	})
+</script>
+
+<textarea
+	class={createClass('w-full resize-none', classProp)}
+	bind:this={el}
+	bind:value
+	{placeholder}
+></textarea>
