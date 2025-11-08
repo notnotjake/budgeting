c937178acde30d7ed05b6d7f617f2e5cb946aba5
Jake Go <jake@notnotjake.com>
2025-11-08 17:36:43 -0500

    update login help link

--- src/routes/(auth)/login/+layout.svelte
+++ src/routes/(auth)/login/+layout.svelte
@@ -9,7 +9,7 @@
 	class="relative flex h-full w-full flex-shrink-0 flex-col items-center justify-center overflow-scroll bg-white"
 >
 	<a
-		class="tracking-tight-md z-50 mx-auto block pt-2 text-center text-base font-medium text-black/55 no-underline transition-all duration-300 hover:text-black/85"
+		class="z-50 mx-auto block pt-2 text-center text-base font-medium tracking-tight-md text-black/55 no-underline transition-all duration-300 hover:text-black/85"
 		href="/">{site.name}</a
 	>
 
@@ -21,7 +21,7 @@
 {#snippet footer()}
 	<div class="mx-auto mb-6 flex w-fit flex-row gap-4 rounded-full px-6 py-3 sm:gap-6">
 		<a
-			href="/privacy"
+			href="/help"
 			class="px-2.5 py-1 text-[0.85rem] tracking-tight text-neutral-500 hover:text-black hover:underline"
 			>Help</a
 		>
