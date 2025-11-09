3c6a1c3585398ce006de2cdfcc33bfbe96938409
Jake Go <jake@notnotjake.com>
2025-11-09 00:07:44 -0500

    railway

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,11 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-import * as schema from './schema'
 import { env } from '$env/dynamic/private'
+import { SQL } from 'bun'
+import { drizzle } from 'drizzle-orm/bun-sql'
+
+import * as schema from './schema'
 
 if (!env.DB_URL) throw new Error('DB_URL is not set')
 
-const client = postgres(env.DB_URL)
+const client = new SQL(env.DB_URL)
 
-export const db = drizzle(client, { schema })
+export const db = drizzle({ client, schema })
