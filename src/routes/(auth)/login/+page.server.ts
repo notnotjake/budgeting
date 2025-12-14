d717b4afd8a0c2495cc32b6859d357dd7211dac2
Jake Go <jake@notnotjake.com>
2025-12-14 13:37:47 -0500

    login route redirects users
    if a user is logged in, /login will redirect them to /app

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,8 +1,13 @@
 import type { ServerLoad } from '@sveltejs/kit'
+import { redirect } from '@sveltejs/kit'
 import Auth from '$lib/server/auth'
 
 export const load: ServerLoad = async (event) => {
 	await Auth.protect.requireSession(event)
+
+	if (event.locals.user) {
+		throw redirect(303, Auth.redirects.afterLogin)
+	}
 
 	return {
 		title: 'Welcome to Spring',
