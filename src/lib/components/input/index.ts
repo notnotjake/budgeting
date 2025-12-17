3631cb0e3cc9494293efd65de07a719f7fac5326
Jake Go <jake@notnotjake.com>
2025-12-17 16:02:18 -0500

    added adapting input

--- src/lib/components/input/index.ts
+++ src/lib/components/input/index.ts
@@ -1,10 +1,34 @@
 export { default as Button } from './button.svelte'
 export { default as Textarea } from './textarea.svelte'
+export { default as InputAdapting } from './input-adapting.svelte'
 
 import InputButton from './button.svelte'
+import InputAdapting from './input-adapting.svelte'
 import Textarea from './textarea.svelte'
 
 export const Input = {
 	Button: InputButton,
-	Textarea: Textarea
+	Textarea: Textarea,
+	InputAdapting: InputAdapting
+}
+
+export type FormInputEvent<T extends Event = Event> = T & {
+	currentTarget: EventTarget & HTMLInputElement
+}
+export type InputEvents = {
+	blur: FormInputEvent<FocusEvent>
+	change: FormInputEvent<Event>
+	click: FormInputEvent<MouseEvent>
+	focus: FormInputEvent<FocusEvent>
+	focusin: FormInputEvent<FocusEvent>
+	focusout: FormInputEvent<FocusEvent>
+	keydown: FormInputEvent<KeyboardEvent>
+	keypress: FormInputEvent<KeyboardEvent>
+	keyup: FormInputEvent<KeyboardEvent>
+	mouseover: FormInputEvent<MouseEvent>
+	mouseenter: FormInputEvent<MouseEvent>
+	mouseleave: FormInputEvent<MouseEvent>
+	paste: FormInputEvent<ClipboardEvent>
+	input: FormInputEvent<InputEvent>
+	wheel: FormInputEvent<WheelEvent>
 }
