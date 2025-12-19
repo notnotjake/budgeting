026c3bba72575d22f88bc56d999b94edf0d40221
Jake Go <jake@notnotjake.com>
2025-12-19 01:34:24 -0500

    updates item and section schemas
    uses uuid for id
    
    section:
    - add icon
    - add column
    - add order for reordering sections in column
    - add archived to archive sections to hide them
    - add collapsed to save collapse status
    
    items:
    - add order
    - add archived

--- src/lib/server/db/schema/sections.ts
+++ src/lib/server/db/schema/sections.ts
@@ -1,8 +1,16 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import { pgTable, text, timestamp, integer, boolean } from 'drizzle-orm/pg-core'
+import { randomUUID } from 'crypto'
 
 export const sections = pgTable('sections', {
-	id: serial('id').primaryKey(),
+	id: text('id')
+		.primaryKey()
+		.$defaultFn(() => randomUUID()),
 	title: text('title').notNull(),
-	createdAt: timestamp('created_at').defaultNow().notNull(),
-	updatedAt: timestamp('updated_at').defaultNow().notNull()
+	icon: text('icon'),
+	column: integer('column').notNull().default(0),
+	order: text('order').notNull(),
+	archived: boolean('archived').notNull().default(false),
+	collapsed: boolean('collapsed').default(false),
+	createdAt: timestamp('created_at', { mode: 'date', withTimezone: true }).defaultNow().notNull(),
+	updatedAt: timestamp('updated_at', { mode: 'date', withTimezone: true }).defaultNow().notNull()
 })
